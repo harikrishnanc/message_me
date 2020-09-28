2e41340398ad1ffe21af23bb1ed36fa1d32252bf
harikrishnanc <hari.911@gmail.com>
2020-09-28 19:54:28 +0530

    committed full code for chat app

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,38 @@
 class SessionsController < ApplicationController
+
+	before_action :logged_in_redirect, only: [:new, :create]
     def new
-    end
+		end
+		
+		def create
+			user = User.find_by(username: session_params[:username])
+
+			if user && user.authenticate(session_params[:password])
+				session[:user_id] = user.id
+				flash[:success] = "You have successfully logged in!"
+				redirect_to root_path
+			else
+				flash.now[:error] = "Something wrong with your login information"
+				render 'new'
+			end
+		end
+
+		def destroy
+			session[:user_id] = nil
+			flash[:success] = "You have successfully logged out!"
+			redirect_to login_path
+
+		end
+
+		private 
+		def session_params
+			params.require(:session).permit(:username, :password)
+		end
+
+		def logged_in_redirect
+			if logged_in?
+				flash[:error] = 'you are already logged in'
+				redirect_to root_path
+			end
+		end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username:"Hari", password: "password1")
+User.create(username:"Vicky", password: "password1")
+User.create(username:"Anish", password: "password1")
+User.create(username:"Rob", password: "password1")
+User.create(username:"Bobby", password: "password1")
